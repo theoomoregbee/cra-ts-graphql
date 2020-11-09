7edd0402b647514286996c087cb39e72a89c6a16
Theophilus Omoregbee <theo4u@ymail.com>
2020-11-08 22:46:24 -0500

    chore: init apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  ApolloProvider,
+  HttpLink,
+  ApolloClient,
+  InMemoryCache,
+} from "@apollo/client";
+
+const graphQLink = new HttpLink({
+  uri: "https://graphql-weather-api.herokuapp.com/",
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: graphQLink,
+  cache,
+  credentials: "include",
+  resolvers: {},
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
